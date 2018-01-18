55d3a417250b2d39c94e6bdca769f5e69fb8c7ee
maestermatt <matthewjames.ng@gmail.com>
2018-01-18 14:57:33 -0800

    Adding the files from Lab 0 and Lab 1

---
+++ encoder_Ng_Kropp_Fitter.py
@@ -0,0 +1,37 @@
+import pyb
+import micropython
+import time
+
+__author__= "Matthew Ng, Eugene Kropp, Yavisht Fitter"
+__date__= "January 18, 2018"
+
+class MotorEncoder:
+    ''' Motor Encoder Class: Has a function to read and reset motor position counter '''
+    def __init__(self):
+        '''Creates a motor encoder by initializing GPIO pins and setting position to zero'''
+        self.pinB6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN, pull=pyb.Pin.PULL_NONE, af=2)
+        self.pinB7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN, pull=pyb.Pin.PULL_NONE, af=2)
+        self.timEncoder = pyb.Timer(4, prescaler=0, period=0xffff)
+        ch1 = self.timEncoder.channel(1, pyb.Timer.ENC_AB, pin=self.pinB6)
+        ch2 = self.timEncoder.channel(2, pyb.Timer.ENC_AB, pin=self.pinB7)
+        self.current_count = self.timEncoder.counter()
+        self.delta_count = 0
+        self.position = 0
+
+    def cal_delta(self):
+        self.last_count = self.current_count
+        self.current_count = self.timEncoder.counter()
+        self.delta_count = self.current_count - self.last_count
+        if abs(self.delta_count) > 32000:
+            self.delta_count -= 65535*self.delta_count/abs(self.delta_count)
+        return int(self.delta_count)
+
+    def zero(self):
+        self.position = 0
+
+    def read(self):
+        self.position = self.position + self.delta_count
+        return self.position
+
+
+        
---
+++ main.py
@@ -0,0 +1,6 @@
+import pyb
+import micropython
+import time
+import encoder_Ng_Kropp_Fitter as encode
+
+enc = encode.MotorEncoder()
