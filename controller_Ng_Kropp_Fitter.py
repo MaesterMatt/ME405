2ad9442bac49878d93ba8822c7c3afb270cf0b61
maestermatt <matthewjames.ng@gmail.com>
2018-01-25 16:37:05 -0800

    modified the files so the classes don't control each other. Instead, all actions come from main

--- controller_Ng_Kropp_Fitter.py
+++ controller_Ng_Kropp_Fitter.py
@@ -5,8 +5,7 @@
 import pyb
 import micropython
 import time
-import encoder_Ng_Kropp_Fitter as encode
-import motor_Ng_Kropp_Fitter as driver
+import utime
 
 __author__= "Matthew Ng, Eugene Kropp, Yavisht Fitter"
 __date__= "January 25,2018"
@@ -16,30 +15,37 @@ class MotorController:
 
     def __init__(self, gain, location):
         '''Creates a motor controller by initializing gain and setpoint. @param gain does something. @param setpoint sets the desired location'''
-        self.enc = encode.MotorEncoder()
-        self.motor = driver.MotorDriver()
         self.kp = gain    ## units are %/encoder unit
         self.setpoint = location
-        self.error = self.setpoint - self.enc.read()
+        self.error = self.setpoint
         self.actuation_signal = self.kp*self.error
+        self.data = []
 
     def set_setpoint(self, location):
         '''This function sets the setpoint'''
+        self.data = []
         self.setpoint = location
 
     def set_gain(self, gain):
         '''This function sets the control gain'''
         self.kp = gain
 
+    def err_calc(self, enc_read):
+        self.error = self.setpoint - enc_read
+        return self.error
+    
     def do_work(self):
         '''This function runs the control algorithm. Returns actuation signal.'''
-        self.error = self.setpoint - self.enc.read()
         self.actuation_signal = self.kp*self.error    ##needs to be b/w 0-100
-        if self.error < 100:
+        if abs(self.error) < 50:
             self.actuation_signal = 0
-        ##utime.sleep_ms(10)
-        self.motor.set_duty_cycle(42)
-        #self.motor.set_duty_cycle(self.actuation_signal)
-        print(self.actuation_signal)
+        
+        self.write_data()
         return self.actuation_signal
+
+    def write_data(self):
+        self.data.append([utime.ticks_ms(), self.actuation_signal])
+
+    def return_data(self):
+        return self.data
 
